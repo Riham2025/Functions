e2895bd20fee6402361af1d76b636b6722257bd2
riham <riham91.alsiyabi@gmail.com>
2025-04-09 14:25:26 +0400

    first day to start the function

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Functions
+﻿using System.ComponentModel.Design;
+
+namespace Functions
 {
     internal class Program
     {
+        public static string EvenOdd(int num1)
+        {
+            int result;
+            result = num1 % 2;
+
+            if (result == 0) {
+                {
+                    return "The Number is Even";
+                } 
+
+               else  
+
+                {
+                    return "The Number is Odd";
+                }
+
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        }
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Enter the number of operation:");
+                Console.WriteLine("1. Even or Odd ");
+                Console.WriteLine("2. Largest of Three Numbers  ");
+                int choice = int.Parse(Console.ReadLine());
+            }
     }
 }
