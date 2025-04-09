302dc9b30810e0a7f44424ccedfa49a6285ef4a1
riham <riham91.alsiyabi@gmail.com>
2025-04-09 14:49:04 +0400

    add change

--- Program.cs
+++ Program.cs
@@ -25,13 +25,13 @@ namespace Functions
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+           
             Console.WriteLine("Enter the number of operation:");
             Console.WriteLine("1. Even or Odd ");
             Console.WriteLine("2. Largest of Three Numbers  ");
             int choice = int.Parse(Console.ReadLine());
 
-            switch (choice)
+            switch (choice) //add function
             {
                 case 1:
                     Console.WriteLine("Enter The Number");
